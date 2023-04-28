6426033cbaa9f085248a32446398670e1091dca2
Rachel-oc <mengting.xie@thoughtworks.com>
2023-04-28 13:46:46 +0800

    first commit

---
+++ src/main/java/com/example/momentsbackend/MomentsBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.momentsbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MomentsBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MomentsBackendApplication.class, args);
+    }
+
+}
