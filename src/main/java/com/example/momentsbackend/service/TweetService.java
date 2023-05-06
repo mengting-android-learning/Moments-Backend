f6564b61e67915aa9eb9f152e676b4648bda6099
Rachel-oc <mengting.xie@thoughtworks.com>
2023-05-06 17:07:00 +0800

    refactor:extract methods

--- src/main/java/com/example/momentsbackend/service/TweetService.java
+++ src/main/java/com/example/momentsbackend/service/TweetService.java
@@ -16,10 +16,13 @@ import com.example.momentsbackend.repository.UserRepository;
 import com.example.momentsbackend.web.dto.TweetResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.util.CollectionUtils;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -32,41 +35,51 @@ public class TweetService {
     private final TweetMapper tweetMapper;
 
     public List<TweetResponse> findAll() {
-        List<TweetResponse> tweets = new ArrayList<>();
         List<TweetEntity> tweetEntities = tweetRepository.findAll();
-        for (TweetEntity tweetEntity : tweetEntities) {
-            Sender sender = null;
-            Optional<UserEntity> senderEntity = userRepository.findById(tweetEntity.getSenderId());
-            if (senderEntity.isPresent()) {
-                sender = userMapper.toDto(senderEntity.get());
-            }
-            List<TweetImageEntity> imageEntities = imageRepository.getImagesByTweetId(tweetEntity.getId());
-            List<TweetImage> images = imageEntities
-                    .stream()
-                    .map(tweetMapper::toDomain)
-                    .toList();
-            List<TweetComment> comments = new ArrayList<>();
-            List<TweetCommentEntity> commentEntities = commentRepository.getCommentsByTweetId(tweetEntity.getId());
-            for (TweetCommentEntity commentEntity : commentEntities) {
-                Sender commentSender = null;
-                Optional<UserEntity> commentSenderEntity = userRepository.findById(commentEntity.getSenderId());
-                TweetComment comment = tweetMapper.toDomain(commentEntity);
-                if (commentSenderEntity.isPresent()) {
-                    commentSender = userMapper.toDto(commentSenderEntity.get());
-                }
-                comment.setSender(commentSender);
-                comments.add(comment);
-            }
-            TweetResponse tweet = new TweetResponse(
-                    tweetEntity.getId(),
-                    tweetEntity.getContent(),
-                    tweetEntity.getCreatedOn(),
-                    images,
-                    sender,
-                    comments
-            );
-            tweets.add(tweet);
-        }
-        return tweets;
+        return CollectionUtils.isEmpty(tweetEntities) ?
+                Collections.emptyList() :
+                tweetEntities.stream()
+                        .map(this::getTweetResponse)
+                        .collect(Collectors.toList());
     }
+
+    private TweetResponse getTweetResponse(TweetEntity tweetEntity) {
+        Sender sender = getSender(tweetEntity.getSenderId());
+        List<TweetImage> images = getImages(tweetEntity.getId());
+        List<TweetComment> comments = getComments(tweetEntity.getId());
+        return new TweetResponse(
+                tweetEntity.getId(),
+                tweetEntity.getContent(),
+                tweetEntity.getCreatedOn(),
+                images,
+                sender,
+                comments
+        );
+    }
+
+    private List<TweetImage> getImages(Long id) {
+        List<TweetImageEntity> imageEntities = imageRepository.getImagesByTweetId(id);
+        return imageEntities.stream()
+                .map(tweetMapper::toDomain)
+                .toList();
+    }
+
+    private Sender getSender(Long senderId) {
+        Optional<UserEntity> senderEntity = userRepository.findById(senderId);
+        return senderEntity.map(userMapper::toDto).orElse(null);
+    }
+
+    private List<TweetComment> getComments(Long id) {
+        List<TweetComment> comments = new ArrayList<>();
+        List<TweetCommentEntity> commentEntities = commentRepository.getCommentsByTweetId(id);
+        commentEntities.forEach(
+                commentEntity -> {
+                    TweetComment comment = tweetMapper.toDomain(commentEntity);
+                    comment.setSender(getSender(commentEntity.getSenderId()));
+                    comments.add(comment);
+                }
+        );
+        return comments;
+    }
+
 }
